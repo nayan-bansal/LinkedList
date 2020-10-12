f7e34be386cc218933ca2a17940329320dbefa78
nayan-bansal <nayan.7991@gmail.com>
2020-10-12 14:40:29 +0530

    Added: Adding Nodes to Linked List

--- MyNode.java
+++ MyNode.java
@@ -1,19 +1,30 @@
 package Linked_List;
 
-public class MyNode<K> {
+public class MyNode<K> implements INode<K>{
+	
 	private K key;
-	private MyNode next;
-
+	private INode next;
+	
 	public MyNode(K key) {
-		this.key = key;
-		this.next = null;
+	this.key = key;
+	this.next = null;
 	}
-
-	public MyNode getNext() {
+	
+	public INode getNext() {
 		return next;
 	}
-
-	public void setNext(MyNode next) {
+	
+	public void setNext(INode next) {
 		this.next = next;
+	}
+
+	@Override
+	public K getKey() {
+		return key;
+	}
+
+	@Override
+	public void setKey(K key) {
+		this.key = (K)   key;
 	}
 }
