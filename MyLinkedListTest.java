cfef21a7994c93e0d0b565405a861268866a998c
nayan-bansal <nayan.7991@gmail.com>
2020-10-12 17:09:26 +0530

    Added: Deleting Last Node

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -74,4 +74,20 @@ public void given3NumbersInsertSecondInBetweenShouldPassTest() {
 				&& myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
+@Test
+	public void givenThirdElementWhenDeletedShouldPassTest() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		INode deletedNode = myLinkedList.popLast();
+		myLinkedList.printMyNode();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(mySecondNode);
+		Assert.assertTrue(result);
+	}
+
 }
