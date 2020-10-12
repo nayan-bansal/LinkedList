c1f170758b008a2dd6c456e7fc3f6dd292d472bc
nayan-bansal <nayan.7991@gmail.com>
2020-10-12 16:59:10 +0530

    Added: Inserting Nodes

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -44,4 +44,18 @@ public void givenNumberShouldbeAtTopappend() {
 	Assert.assertTrue(res);
 }
 
+@Test
+public void given3NumbersInsertSecondInBetweenShouldPassTest() {
+	MyNode<Integer> myFirstNode = new MyNode<>(56);
+	MyNode<Integer> mySecondNode = new MyNode<>(30);
+	MyNode<Integer> myThirdNode = new MyNode<>(70);
+	MyLinkedList myLinkedList = new MyLinkedList();
+	myLinkedList.add(myFirstNode);
+	myLinkedList.append(myThirdNode);
+	myLinkedList.insert(myFirstNode, mySecondNode);
+	myLinkedList.printnodes();
+	boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+			&& myLinkedList.tail.equals(myThirdNode);
+	Assert.assertTrue(result);
+}
 }
