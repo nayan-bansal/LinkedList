c11cd82648c0d601ec8f7347c1a0bb674f6bf504
nayan-bansal <nayan.7991@gmail.com>
2020-10-12 14:46:56 +0530

    Added: Adding Nodes to Linked List

---
+++ MyLinkedListTest.java
@@ -0,0 +1,30 @@
+package Linked_List;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MyLinkedListTest {
+
+@Test 
+public void givenNumberShouldbeAtTop() {
+	MyNode<Integer> myFirstNode = new MyNode<>(56);
+	MyNode<Integer> mySecondNode = new MyNode<>(30);
+	MyNode<Integer> myThirdNode = new MyNode<>(70);
+	
+	MyLinkedList linkedlist = new MyLinkedList();
+	linkedlist.add(myFirstNode);
+	linkedlist.add(mySecondNode);
+	linkedlist.add(myThirdNode);
+	
+	boolean result  = linkedlist.head.equals(myThirdNode) && linkedlist.head.getNext().equals(mySecondNode) && linkedlist.tail.equals(myFirstNode);
+	
+	Assert.assertTrue(result);
+	
+	
+}
+
+}
+
