bab83d6153683fe304ebfd16b7922bbeccd1fab7
nayan-bansal <nayan.7991@gmail.com>
2020-10-12 17:05:06 +0530

    Added: Deleting Nodes

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -58,4 +58,20 @@ public void given3NumbersInsertSecondInBetweenShouldPassTest() {
 			&& myLinkedList.tail.equals(myThirdNode);
 	Assert.assertTrue(result);
 }
+
+@Test
+	public void givenFirstElementWhenDeletedShouldPassTest() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		INode deletedNode = myLinkedList.pop();
+		myLinkedList.printMyNode();
+		boolean result = myLinkedList.head.equals(mySecondNode) && myLinkedList.head.getNext().equals(myThirdNode)
+				&& myLinkedList.tail.equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
 }
